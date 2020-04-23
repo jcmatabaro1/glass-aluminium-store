d165d4e2ae15210913a6596a9f2e19c5c9489b87
JC Birindwa Matabaro <jcbm81@gmail.com>
2020-04-23 18:38:12 +0200

    paypal button added

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -12,14 +12,14 @@ export default class Cart extends Component {
       <section>
         <ProductConsumer>
           {value => {
-            const { cart } = value;
+            const { cart } = value;  
             if (cart.length > 0) {
               return (
                 <React.Fragment>
                   <Title name="your" title="cart" />
                   <CartColumns />
                   <CartLIst value={value} />
-                  <CartTotals value={value} />
+                  <CartTotals value={value} history={this.props.history}/>
                 </React.Fragment>
               );
             } else {
--- src/component/Cart/CartTotals.js
+++ src/component/Cart/CartTotals.js
@@ -1,6 +1,8 @@
 import React from "react";
 import {Link} from "react-router-dom";
-export default function CartTotals({ value }) {
+import PayPalButton from "./PayPalButton";
+
+export default function CartTotals({ value,history }) {
   const { cartSubtotal, cartTax, cartTotal, clearCart } = value;
   return (
     <React.Fragment>
@@ -24,20 +26,25 @@ export default function CartTotals({ value }) {
                 <span className="text-title">
                     subtotal :
                 </span>
-                <strong>$ {cartSubtotal}</strong>
+                <strong>R {cartSubtotal}</strong>
             </h5>
                       <h5>
                           <span className="text-title">
                               Tax :
                 </span>
-                          <strong>$ {cartTax}</strong>
+                          <strong>R {cartTax}</strong>
                       </h5>
                       <h5>
                           <span className="text-title">
                               total :
                 </span>
-                          <strong>$ {cartTotal}</strong>
+                          <strong>R {cartTotal}</strong>
                       </h5>
+                      <PayPalButton  
+                      total={cartTotal} 
+                      clearCart={clearCart}
+                      history={history}
+                      />
           </div>
         </div>
       </div>
